51f984bada3b00ec9d3f07437b09964378eb6ada
BlackLotusJ <jerrykiche61@gmail.com>
2024-01-24 11:39:48 +0300

    creted forloop.cpp adn corrected the error

---
+++ forloop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	string arr[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+	int a, b;
+
+	cin>>a>>b;
+	for (int i = a; i <= b; i++)
+	{
+		if (i<=9)
+			cout<<arr[i]<<endl;
+		else if (i%2 == 0)
+			cout<<"even"<<endl;
+		else
+			cout<<"odd"<<endl;
+	}
+	return (0);
+}
