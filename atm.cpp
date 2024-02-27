5b31b0cdc921ba82f6869be8e1cb97bdbde0c2b2
BlackLotusJ <jerrykiche61@gmail.com>
2024-02-27 20:42:43 +0300

    updated

---
+++ atm.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int pin;
+	cout<<"-------------------------------\n\t\tATM\n-------------------------------\n";
+	cout<<"WELCOME!\n";
+	cout<< "Enter 4-digit PIN:";
+	cin>>pin;
+}
