f98a4a25bfabb11e2eaeb5a45efb028b35cc0ffe
BlackLotusJ <jerrykiche61@gmail.com>
2024-02-29 11:43:33 +0300

    updated

--- ATM/atm.cpp
+++ ATM/atm.cpp
@@ -1,11 +1,78 @@
 #include <iostream>
-using namespace std;
+#include <fstream>
+#include "main.h"
+
+
+struct UserData 
+{
+	int user_id;
+	string name;
+	int pin;
+	double account_balance;
+};
+
+// Comparator function for sorting UserData objects by name
+bool compareByName(const UserData& a, const UserData& b) {
+    return a.name < b.name;
+}
+
+// Binary search function to find a user by name
+int binarySearch(const vector<UserData>& users, const string& target) {
+    int left = 0;
+    int right = users.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (users[mid].name == target) {
+            return mid; // User found at index mid
+        }
+        else if (users[mid].name < target) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+
+    return -1; // User not found
+}
 
 int main()
 {
+	std::cout<<"-------------------------------\n\t\tATM\n-------------------------------\n";
+	std::cout<<"WELCOME!\n";
+	std::fstream file("list.txt");
+	if (!file.is_open())
+	{
+		std::err << "ERROR!" << std::endl;
+		return 1;
+	}
+
+	vector<UserData> users;
+	int user_id;
+	string name;
 	int pin;
-	cout<<"-------------------------------\n\t\tATM\n-------------------------------\n";
-	cout<<"WELCOME!\n";
-	cout<< "Enter 4-digit PIN:";
-	cin>>pin;
+	double account_balance;
+
+	while (file >> user_id >> name >> pin >> account_balance)
+	{
+		users.push_back({user_id, name, pin, account_balance});
+	}
+	file.close();
+
+	std::cout<< "Enter Username: ";
+	std::cin >> name;
+	int index = binarySearch(users, name);
+
+	if (index != -1)
+	{
+		std::cout << "User ID: " << users[index].user_id << std::endl;
+	}
+	else
+	{
+		std::cout << "User not Found" << std::endl;
+	}
+
+	return (0);
 }
---
+++ ATM/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+
+int binarySearch(const vector<UserData>& users, const string& target);
+
+#endif
